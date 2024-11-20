c9b71837fb2d06c2a325a2a9a47e66d9517ea77b
Florent Bonamis <florent.bonamis@sfpd.fgov.be>
2024-11-20 13:58:36 +0100

    native setup

--- adapter-tfl/src/main/java/be/bonaf/publictransport/adapter/NativeConfiguration.java
+++ adapter-tfl/src/main/java/be/bonaf/publictransport/adapter/NativeConfiguration.java
@@ -1,14 +1,24 @@
+// NativeConfiguration.java
 package be.bonaf.publictransport.adapter;
-
 
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
-
+import org.openapitools.client.model.*;
+import org.springframework.aot.hint.MemberCategory;
 
 public class NativeConfiguration implements RuntimeHintsRegistrar {
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-        ReflectionConfiguration reflectionConfiguration = new ReflectionConfiguration();
-        reflectionConfiguration.registerPackageClasses(hints, "org.openapitools.client.model");
+        register(hints, TflApiPresentationEntitiesArrivalDeparture.class);
+        register(hints, TflApiPresentationEntitiesPredictionTiming.class);
+    }
+
+    private void register(RuntimeHints hints, Class<?> type) {
+        hints.reflection().registerType(
+                type,
+                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+                MemberCategory.INVOKE_PUBLIC_METHODS,
+                MemberCategory.DECLARED_FIELDS
+        );
     }
 }
--- adapter-tfl/src/main/java/be/bonaf/publictransport/adapter/ReflectionConfiguration.java
+++ adapter-tfl/src/main/java/be/bonaf/publictransport/adapter/ReflectionConfiguration.java
@@ -1,3 +1,4 @@
+/*
 package be.bonaf.publictransport.adapter;
 
 import org.springframework.aot.hint.MemberCategory;
@@ -22,3 +23,4 @@ public class ReflectionConfiguration {
         }
     }
 }
+*/
