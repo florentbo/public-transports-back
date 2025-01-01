5c4c0cfae9bdf847a04388572b4062aafef019b1
Florent Bonamis <florent.bonamis@sfpd.fgov.be>
2025-01-01 09:23:57 +0100

    bru handling multiple line issue

--- adapter-bru/src/main/java/be/bonaf/publictransport/adapter/configuration/BruConfiguration.java
+++ adapter-bru/src/main/java/be/bonaf/publictransport/adapter/configuration/BruConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.*;
 
 import java.time.Clock;
 import java.util.List;
+import java.util.Map;
 
 @Configuration
 @AllArgsConstructor
@@ -17,23 +18,42 @@ public class BruConfiguration {
   @Bean
   @BruArrivalTimeService
   public ArrivalTimeService bruArrivalTimeService() {
-    String apiKey = "aa811295bf39fa65cabc9db92a726bcaf373eb062201ec5b2d4abec6";
-    String baseUrl = "https://stibmivb.opendatasoft.com/api/explore/v2.1";
-    var client = new StibMivbClient(baseUrl, apiKey);
-    Mapper mapper = new Mapper(Clock.systemDefaultZone());
+    return new ArrivalTimeService() {
+      @Override
+      public List<ArrivalTime> arrivalTimes(Journey journey) {
 
-    return journey -> {
-      List<Journey.Trip> trips = journey.trips();
-      WaitingTimes waitingTimes =
-          client.waitingTimes(
-              trips.stream()
-                  .map(Journey.Trip::startingPoint)
-                  .map(Journey.Trip.TransportStation::stationId)
-                  .toList());
-      trips.stream().map(Journey.Trip::line);
+        String apiKey = "aa811295bf39fa65cabc9db92a726bcaf373eb062201ec5b2d4abec6";
+        String baseUrl = "https://stibmivb.opendatasoft.com/api/explore/v2.1";
+        var client = new StibMivbClient(baseUrl, apiKey);
+        Mapper mapper = new Mapper(Clock.systemDefaultZone());
 
+        List<Journey.Trip> trips = journey.trips();
+        WaitingTimes waitingTimes =
+            client.waitingTimes(
+                trips.stream()
+                    .map(Journey.Trip::startingPoint)
+                    .map(Journey.Trip.TransportStation::stationId)
+                    .toList());
+        return mapper.from(waitingTimes);
+      }
 
-      return mapper.from(waitingTimes);
+      @Override
+      public Map<String, List<ArrivalTime>> arrivalTimesPerLine(Journey journey) {
+
+        String apiKey = "aa811295bf39fa65cabc9db92a726bcaf373eb062201ec5b2d4abec6";
+        String baseUrl = "https://stibmivb.opendatasoft.com/api/explore/v2.1";
+        var client = new StibMivbClient(baseUrl, apiKey);
+        List<Journey.Trip> trips = journey.trips();
+        WaitingTimes waitingTimes =
+            client.waitingTimes(
+                trips.stream()
+                    .map(Journey.Trip::startingPoint)
+                    .map(Journey.Trip.TransportStation::stationId)
+                    .toList());
+
+        Mapper mapper = new Mapper(Clock.systemDefaultZone());
+        return mapper.fromMultipleLines(waitingTimes);
+      }
     };
   }
 }
--- domain/src/main/java/be/bonaf/publictransport/domain/journey/ArrivalTimeService.java
+++ domain/src/main/java/be/bonaf/publictransport/domain/journey/ArrivalTimeService.java
@@ -1,7 +1,12 @@
 package be.bonaf.publictransport.domain.journey;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ArrivalTimeService {
   List<ArrivalTime> arrivalTimes(Journey journey);
+
+  default Map<String, List<ArrivalTime>> arrivalTimesPerLine(Journey journeys) {
+    return Map.of();
+  }
 }
