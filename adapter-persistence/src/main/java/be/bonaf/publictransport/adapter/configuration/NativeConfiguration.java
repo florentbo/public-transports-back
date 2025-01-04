a9436d81b2042702540f472d609bcc98a75a1d66
Florent Bonamis <florent.bonamis@sfpd.fgov.be>
2025-01-04 23:10:09 +0100

    moving the journey to the persistencte layer to have more control

--- adapter-persistence/src/main/java/be/bonaf/publictransport/adapter/configuration/NativeConfiguration.java
+++ adapter-persistence/src/main/java/be/bonaf/publictransport/adapter/configuration/NativeConfiguration.java
@@ -1,5 +1,6 @@
 package be.bonaf.publictransport.adapter.configuration;
 
+import be.bonaf.publictransport.domain.journey.*;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
@@ -8,6 +9,14 @@ public class NativeConfiguration implements RuntimeHintsRegistrar {
   @Override
   public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
     register(hints, JourneyReader.JourneyIdKeyDeserializer.class);
+    register(hints, Journey.class);
+    register(hints, Journey.JourneyId.class);
+    register(hints, Journey.TransportType.class);
+    register(hints, Journey.City.class);
+    register(hints, Location.class);
+    register(hints, LocationId.class);
+    register(hints, Journey.Trip.class);
+    register(hints, Journey.Trip.TransportStation.class);
   }
 
   private void register(RuntimeHints hints, Class<?> type) {
