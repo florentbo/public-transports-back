6fd6f3da2735ea579f34b608bedcd12010bf1be7
Florent Bonamis <florent.bonamis@sfpd.fgov.be>
2025-01-04 22:00:06 +0100

    moving the journey to the persistencte layer to have more control

--- adapter-persistence/src/main/java/be/bonaf/publictransport/adapter/configuration/InMemoryConfiguration.java
+++ adapter-persistence/src/main/java/be/bonaf/publictransport/adapter/configuration/InMemoryConfiguration.java
@@ -7,7 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.*;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 
@@ -30,24 +35,42 @@ public class InMemoryConfiguration {
         return getJourneys().values().stream().toList();
       }
 
+      private InputStream getFileContent(String url) {
+
+        HttpClient client =
+            HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(10))
+                .build()  ;
+
+          HttpRequest request =
+              HttpRequest.newBuilder()
+                  .uri(URI.create(url))
+                  .timeout(Duration.ofSeconds(10))
+                  .header("User-Agent", "Java 21 Client")
+                  .GET()
+                  .build();
+
+          try {
+            return client.send(request, HttpResponse.BodyHandlers.ofInputStream()).body();
+          } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Failed to fetch file content from " + url, e);
+          }
+        }
+
+
       @Override
       public Journey journey(Journey.JourneyId journeyId) {
         return getJourneys().get(journeyId);
       }
 
       private Map<Journey.JourneyId, Journey> getJourneys() {
-        try {
-          URL url = new URL("https://static.staticsave.com/journeys/journeys.json");
-          try (var stream = url.openStream()) {
-            return JourneyReader.readValue(stream);
-          }
-        } catch (MalformedURLException e) {
-          log.error("Error while reading url: ", e);
-          throw new RuntimeException(e);
-        } catch (IOException e) {
-          log.error("Error while opening stream: ", e);
-          throw new RuntimeException(e);
-        }
+          // http client to get file content:
+
+          String url1 = "https://verdant-blini-222724.netlify.app/journeys.json";
+
+          return JourneyReader.readValue(getFileContent(url1));
+
       }
     };
   }
