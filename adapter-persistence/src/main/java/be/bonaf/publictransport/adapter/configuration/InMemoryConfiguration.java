63c24a7af89947093c9449e7fe1c8f8af346f962
Florent Bonamis <florent.bonamis@sfpd.fgov.be>
2025-02-15 09:49:38 +0100

    Code cleaning

--- adapter-persistence/src/main/java/be/bonaf/publictransport/adapter/configuration/InMemoryConfiguration.java
+++ adapter-persistence/src/main/java/be/bonaf/publictransport/adapter/configuration/InMemoryConfiguration.java
@@ -9,10 +9,7 @@ import org.springframework.context.annotation.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.*;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
+import java.net.http.*;
 import java.util.List;
 import java.util.Map;
 
@@ -36,28 +33,19 @@ public class InMemoryConfiguration {
       }
 
       private InputStream getFileContent(String url) {
-
-        HttpClient client =
-            HttpClient.newBuilder()
-                .followRedirects(HttpClient.Redirect.NORMAL)
-                .connectTimeout(Duration.ofSeconds(10))
-                .build()  ;
-
+        try (var client = HttpClient.newBuilder().build()) {
           HttpRequest request =
               HttpRequest.newBuilder()
                   .uri(URI.create(url))
-                  .timeout(Duration.ofSeconds(10))
                   .header("User-Agent", "Java 21 Client")
                   .GET()
                   .build();
 
-          try {
-            return client.send(request, HttpResponse.BodyHandlers.ofInputStream()).body();
-          } catch (IOException | InterruptedException e) {
-            throw new RuntimeException("Failed to fetch file content from " + url, e);
-          }
+          return client.send(request, HttpResponse.BodyHandlers.ofInputStream()).body();
+        } catch (IOException | InterruptedException e) {
+          throw new RuntimeException("Failed to fetch file content from " + url, e);
         }
-
+      }
 
       @Override
       public Journey journey(Journey.JourneyId journeyId) {
@@ -65,12 +53,8 @@ public class InMemoryConfiguration {
       }
 
       private Map<Journey.JourneyId, Journey> getJourneys() {
-          // http client to get file content:
-
-          String url1 = "https://verdant-blini-222724.netlify.app/journeys.json";
-
-          return JourneyReader.readValue(getFileContent(url1));
-
+        String url = "https://verdant-blini-222724.netlify.app/journeys.json";
+        return JourneyReader.readValue(getFileContent(url));
       }
     };
   }
--- adapter-tfl/src/main/java/be/bonaf/publictransport/adapter/NativeConfiguration.java
+++ adapter-tfl/src/main/java/be/bonaf/publictransport/adapter/NativeConfiguration.java
@@ -4,15 +4,25 @@ import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.openapitools.client.model.*;
 import org.springframework.aot.hint.MemberCategory;
+import org.springframework.lang.Nullable;
+
+import java.util.List;
 
 public class NativeConfiguration implements RuntimeHintsRegistrar {
     @Override
-    public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-        register(hints, TflApiPresentationEntitiesArrivalDeparture.class);
-        register(hints, TflApiPresentationEntitiesPredictionTiming.class);
+    public void registerHints(@Nullable RuntimeHints hints, ClassLoader classLoader) {
+        List<Class<?>> classes = List.of(
+                TflApiPresentationEntitiesArrivalDeparture.class,
+                TflApiPresentationEntitiesPredictionTiming.class);
+        registerHints(hints, classes );
     }
 
-    private void register(RuntimeHints hints, Class<?> type) {
+    void registerHints(@Nullable RuntimeHints hints, List<Class<?>> classes) {
+        classes.forEach(clazz -> register(hints, clazz));
+    }
+
+    private void register(@Nullable RuntimeHints hints, Class<?> type) {
+        assert hints != null;
         hints.reflection().registerType(
                 type,
                 MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
