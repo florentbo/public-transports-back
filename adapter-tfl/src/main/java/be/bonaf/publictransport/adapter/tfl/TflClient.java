a6bd84bb2fa05a3472b6389c612eac6597f72c62
Florent Bonamis <florent.bonamis@sfpd.fgov.be>
2025-02-17 21:36:31 +0100

    start using client

--- adapter-tfl/src/main/java/be/bonaf/publictransport/adapter/tfl/TflClient.java
+++ adapter-tfl/src/main/java/be/bonaf/publictransport/adapter/tfl/TflClient.java
@@ -1,9 +1,13 @@
 package be.bonaf.publictransport.adapter.tfl;
 
+import be.bonaf.publictransport.domain.journey.ArrivalTime;
 import be.bonaf.publictransport.domain.schedule.Departure;
 
 import java.util.List;
+import java.util.Map;
 
 public interface TflClient {
   List<Departure> departures(String startingNaptanId, String destinationNaptanId, String lineId);
+  Map<String, List<ArrivalTime>> arrivals(String stopId);
+
 }
